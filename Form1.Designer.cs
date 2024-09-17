f7bd1f0d465d3d21e1fc562ad574160700d7a89b
Allicia Morete <allicia.morete4300@gmail.com>
2024-09-17 08:54:31 -0300

    Montagem Tela de Loading

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            progressBar1 = new ProgressBar();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(-7, 425);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(815, 35);
+            progressBar1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Image = Properties.Resources.cachorro;
+            pictureBox1.Location = new Point(612, 243);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(196, 126);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(642, 381);
+            label1.Name = "label1";
+            label1.Size = new Size(146, 38);
+            label1.TabIndex = 2;
+            label1.Text = "Loading...";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(progressBar1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calculadora";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ProgressBar progressBar1;
+        private PictureBox pictureBox1;
+        private Label label1;
     }
 }
